840fff4f99cd5e603252dbcb10116125bffd943b
alfonsojk <alfonso.juarezkelly@gmail.com>
2021-04-27 13:09:03 -0500

    movies setup

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 //redux configuration
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
+import movieReducer from "../features/movie/movieSlice";
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    movie: movieReducer,
   },
   middleware: getDefaultMiddleware({
     serializableCheck: false,
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,31 @@ import Originals from './Originals';
 import Recommends from './Recommends';
 import Trending from './Trending';
 import Viewers from './Viewers';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import db from '../firebase';
+import { setMovies } from '../features/movie/movieSlice';
+import { selectUserName } from '../features/user/userSlice';
 
 const Home = (props) => {
+
+  const dispatch = useDispatch()
+  const userName = useSelector(selectUserName);
+  let recommends = [];
+  let newDisney = [];
+  let originals = [];
+  let trending = [];
+
+  useEffect(() => {
+    db.collection('movies').onSnapshot((snapshot) =>{
+      snapshot.docs.map((doc) =>{
+        switch(doc.data().type){
+          case 'recommend'
+        }
+      })
+    })
+  })
+
     return  (   
     <Container>
         <ImgSlider />
@@ -15,7 +38,7 @@ const Home = (props) => {
         <Recommends />
         <NewDisney />
         <Originals />
-        <Trending />
+        <Trending /> 
     </Container>
     );
 };
